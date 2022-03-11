9a2ef3059f9e2918b24c95090317f822872b601b
Eugene-Baichuk <evgeny.baichuk@gmail.com>
2022-03-11 23:45:10 +0200

    lesson-10-events-task1-done

---
+++ lesson-10-events/task1.js
@@ -0,0 +1,272 @@
+'use strict';
+(function () {
+  var formArr = [{
+      name: "new-form",
+      className: "form",
+      method: "POST",
+      action: "http://fe.it-academy.by/TestForm.php",
+    },
+    {
+      elemType: 'header',
+      className: 'form__header',
+      textContent: 'Для внесения вашего сайта в каталог, заполните форму:',
+    },
+    {
+      elemType: 'text1line',
+      textContent: 'Разработчики:',
+      htmlFor: 'developer',
+    },
+    {
+      id: 'developer',
+      type: 'text',
+      name: 'developer',
+      maxlength: '300',
+      style: 'width: 453px',
+    },
+    {
+      elemType: 'text2line',
+      textContent: 'Название сайта: ',
+      htmlFor: 'website-name',
+    },
+    {
+      id: 'website-name',
+      type: 'text',
+      name: 'developer',
+      maxlength: '100',
+      style: 'width: 453px',
+    },
+    {
+      elemType: 'text3line',
+      textContent: 'URL сайта: ',
+      htmlFor: 'website-url',
+    },
+    {
+      id: 'website-url',
+      type: 'text',
+      name: 'url',
+      maxlength: '200',
+      style: 'width: 300px',
+    },
+    {
+      elemType: 'text4line',
+      textContent: 'Дата запуска сайта: ',
+      htmlFor: 'launch-date',
+    },
+    {
+      id: 'launch-date',
+      type: 'text',
+      name: 'launch-date',
+      maxlength: '4',
+      style: 'width: 80px',
+    },
+    {
+      elemType: 'text5line',
+      textContent: 'Посетителей в сутки: ',
+      htmlFor: 'visitors',
+    },
+    {
+      id: 'visitors',
+      type: 'text',
+      name: 'visitors',
+      maxlength: '10',
+      style: 'width: 80px',
+    },
+    {
+      elemType: 'text6line',
+      textContent: 'E-mail для связи: ',
+      htmlFor: 'email',
+    },
+    {
+      id: 'email',
+      type: 'text',
+      name: 'email',
+      maxlength: '20',
+      style: 'width: 200px',
+    },
+    {
+      elemType: 'select7line',
+      textContent: 'Рубрика каталога: ',
+      htmlFor: 'rubric',
+    },
+    {
+      id: 'rubric',
+      name: 'select',
+      style: 'width: 204px',
+      option: ['бытовая техника', 'здоровье', 'домашний уют'],
+    },
+    {
+      elemType: 'radio8line',
+      textContent: 'Размещение: ',
+      htmlFor: 'placement',
+    },
+    {
+      id: 'placement',
+      type: 'radio',
+      name: 'placement',
+      textContent: ['бесплатное', 'платное', 'VIP'],
+    },
+    {
+      elemType: 'checkbox9line',
+      textContent: 'Разрешить отзывы: ',
+      htmlFor: 'reviews',
+    },
+    {
+      id: 'reviews',
+      type: 'checkbox',
+      name: 'reviews',
+    },
+    {
+      elemType: 'textarea10line',
+      textContent: 'Описание сайта: ',
+      htmlFor: 'description',
+    },
+    {
+      elemType: 'textarea10line',
+      textContent: 'Описание сайта: ',
+      htmlFor: 'description',
+    },
+    {
+      elemType: 'textarea',
+      id: 'description',
+      className: 'form__textarea',
+      name: 'description',
+    },
+    {
+      type: 'submit',
+      value: 'Опубликовать',
+      style: 'width: 110px;',
+    },
+  ];
+
+  function Form(arr) {
+    this.arr = arr;
+    this.htmlForm = document.querySelector('form');
+    this.formClone = this.htmlForm.cloneNode(true);
+  }
+  //находим нжный объект в массиве formArr 
+  Form.prototype.findCurrentObj = function (attrVal) {
+    var result;
+    this.arr.forEach(elem => {
+      for (var key in elem) {
+        if (elem[key] === attrVal) {
+          result = elem;
+        }
+      }
+    });
+    return result;
+  };
+  //если в объекте есть массив - получаем его
+  Form.prototype.iterateCurrentObj = function (obj) {
+    var keys = Object.keys(obj);
+    var result;
+    keys.forEach(item => {
+      if (typeof obj[item] === "object") {
+        result = obj[item];
+      }
+    });
+    return result;
+  };
+  // записываем аттрибуты из formArr в созданный элемент
+  Form.prototype.setAttrByValue = function (elem, attrVal) {
+    var currentObj = this.findCurrentObj(attrVal);
+    var keys = Object.keys(currentObj);
+    keys.forEach(item => {
+      if (typeof currentObj[item] !== 'object') {
+        elem[item] = currentObj[item];
+      }
+    });
+  };
+  //рендерим всю форму на страницу
+  Form.prototype.render = function () {
+    this.htmlForm.replaceWith(this.formClone);
+  };
+
+  var form = new Form(formArr);
+  var formClone = form.formClone;
+
+  function ChildElem(arr) {
+    Form.call(this, arr);
+  }
+
+  function Select(arr) {
+    Form.call(this, arr);
+  }
+
+  function Radio(arr) {
+    Form.call(this, arr);
+  }
+  // наследуемся и для каждого типа элементов задаем свою функцию renderElem, которая рендерит элемент в formClone
+  inherit(ChildElem, Form, function (elem, parent, attrVal) {
+    var element = document.createElement(elem);
+    this.setAttrByValue(element, attrVal);
+    parent.appendChild(element);
+  });
+
+  inherit(Select, Form, function (elem, parent, attrVal) {
+    var currentObj = this.findCurrentObj(attrVal);
+    var textArr = this.iterateCurrentObj(currentObj);
+    var element = document.createElement(elem);
+    this.setAttrByValue(element, attrVal);
+    textArr.forEach(item => {
+      var childElem = document.createElement('option');
+      childElem.textContent = item;
+      element.appendChild(childElem);
+    });
+    parent.appendChild(element);
+  });
+
+  inherit(Radio, Form, function (elem, parent, attrVal) {
+    var currentObj = this.findCurrentObj(attrVal);
+    var textArr = this.iterateCurrentObj(currentObj);
+    var element = document.createElement(elem);
+    textArr.forEach(item => {
+      childElem.renderElem('input', element, 'radio');
+      var span = document.createElement('span');
+      span.textContent = item;
+      element.appendChild(span);
+    });
+    parent.appendChild(element);
+  });
+
+  var select = new Select(formArr);
+  var radio = new Radio(formArr);
+  var childElem = new ChildElem(formArr);
+
+  form.setAttrByValue(formClone, 'POST'); // задаем аттрибуты тегу form
+  //задаем параметры элементов формы
+  var formElems = [
+    [childElem, 'h4', formClone, 'header'],
+    [childElem, 'label', formClone, 'text1line'],
+    [childElem, 'input', formClone, '300'],
+    [childElem, 'label', formClone, 'text2line'],
+    [childElem, 'input', formClone, '100'],
+    [childElem, 'label', formClone, 'text3line'],
+    [childElem, 'input', formClone, '200'],
+    [childElem, 'label', formClone, 'text4line'],
+    [childElem, 'input', formClone, '4'],
+    [childElem, 'label', formClone, 'text5line'],
+    [childElem, 'input', formClone, '10'],
+    [childElem, 'label', formClone, 'text6line'],
+    [childElem, 'input', formClone, '20'],
+    [childElem, 'label', formClone, 'select7line'],
+    [select, 'select', formClone, 'select'],
+    [childElem, 'label', formClone, 'radio8line'],
+    [radio, 'div', formClone, 'radio'],
+    [childElem, 'label', formClone, 'checkbox9line'],
+    [childElem, 'input', formClone, 'checkbox'],
+    [childElem, 'label', formClone, 'textarea10line'],
+    [childElem, 'textarea', formClone, 'textarea'],
+    [childElem, 'input', formClone, 'submit']
+  ];
+  // рендерим все элементы в formClone
+  formElems.forEach(item => {
+    item[0].renderElem(item[1], item[2], item[3]);
+  });
+  form.render(); // рендерим form  на страницу
+
+  function inherit(child, parent, func) {
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+    child.prototype.renderElem = func;
+  }
+})();
