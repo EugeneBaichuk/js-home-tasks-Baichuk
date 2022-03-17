b77e116f75787a284e47d926f07b8ef547d91393
Eugene-Baichuk <evgeny.baichuk@gmail.com>
2022-03-17 20:38:03 +0200

    lesson-11-events-fix-and-comments

--- lesson-11-events/script.js
+++ lesson-11-events/script.js
@@ -4,14 +4,19 @@ var container = document.querySelector('div');
 container.style.position = 'relative';
 var imgs = document.querySelectorAll('img');
 var startX = 0;
-
-
+var startY = 0;
+//устанавливаем стартовое положение картинок 
 imgs.forEach(img => {
   img.style.position = 'absolute';
   img.classList.add('img');
   img.style.left = (10 + startX) + 'px';
   startX += (10 + img.offsetWidth);
-  img.style.top = 10 + 'px';
+  img.style.top = (10 + startY) + 'px';
+  if ((startX + img.offsetWidth) > container.clientWidth) {
+    startX = 0;
+    startY += (10 + img.offsetHeight);
+  }
+  console.dir(container);
 });
 
 var down = false;
@@ -27,33 +32,35 @@ container.addEventListener('mousedown', event => {
   }
 });
 
-container.addEventListener('mouseup', event => {
-  event.preventDefault();
-  event.target.style.cursor = 'default';
-  down = false;
-  imgs.forEach(item => {
-    item.style.zIndex = 1;
-  });
-  event.target.style.zIndex = 0;
-});
-
 container.addEventListener('mousemove', event => {
   if (down) {
     if (event.target.classList.contains('img')) {
       event.preventDefault();
-      // координаты клика внутри container  
+      // находим координаты клика внутри container и меняем положение img  
       var insideContainerX = event.pageX - event.currentTarget.offsetLeft - xInside;
       var insideContainerY = event.pageY - event.currentTarget.offsetTop - yInside;
       event.target.style.left = insideContainerX + 'px';
       event.target.style.top = insideContainerY + 'px';
-      // при столкновении container становится красным
+      // при столкновении с краем - border у контейнера становится красным
       makeContainerLimits('clientHeight', insideContainerY, 'top');
       makeContainerLimits('clientWidth', insideContainerX, 'left');
-      // container обычный
-      if ((container.classList.value && insideContainerY > 0 && insideContainerY < container.clientHeight - event.target.clientHeight) && (container.classList.value && insideContainerX > 0 && insideContainerX < container.clientWidth - event.target.clientWidth)) container.classList.remove('alarm');
+      // border снова обычный
+      if ((insideContainerY > 0 && insideContainerY < container.clientHeight - event.target.clientHeight) && (insideContainerX > 0 && insideContainerX < container.clientWidth - event.target.clientWidth)) {
+        container.classList.remove('alarm');
+      }
     }
   }
 
+  container.addEventListener('mouseup', event => {
+    event.preventDefault();
+    down = false;
+    event.target.style.cursor = 'default';
+    imgs.forEach(item => {
+      item.style.zIndex = 1;
+    });
+    event.target.style.zIndex = 0; // при mouseup картинка падает на самый нижний слой
+  });
+  // ограничение области перемещения картинок размером контейнера
   function makeContainerLimits(limit, position, side) {
     var heightLimit = container[limit] - event.target[limit];
     if (position <= 0) {
@@ -65,7 +72,7 @@ container.addEventListener('mousemove', event => {
     }
   }
 });
-
+// два класса для плавной анимации в обе стороны
 container.addEventListener('mouseover', () => {
   if (container.classList.contains('end')) {
     container.classList.remove('end');
