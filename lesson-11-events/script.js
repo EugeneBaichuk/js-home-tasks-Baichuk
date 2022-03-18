c17ff522b73d86ae50ac173e3ba187f9bfcea88d
Eugene-Baichuk <evgeny.baichuk@gmail.com>
2022-03-18 15:26:11 +0200

    naming-fixed

--- lesson-11-events/script.js
+++ lesson-11-events/script.js
@@ -19,26 +19,28 @@ imgs.forEach(img => {
 
 var down = false;
 var xInside, yInside;
+var img;
 container.addEventListener('mousedown', function (event) {
-  if (event.target.tagName === 'IMG') {
+  img = event.target;
+  if (img.tagName === 'IMG') {
     event.preventDefault();
     down = true;
-    event.target.style.cursor = 'pointer';
-    event.target.style.zIndex = 5;
-    xInside = event.pageX - event.target.offsetLeft - this.offsetLeft; //координаты клика внутри img
-    yInside = event.pageY - event.target.offsetTop - this.offsetTop;
+    img.style.cursor = 'pointer';
+    img.style.zIndex = 5;
+    xInside = event.pageX - img.offsetLeft - this.offsetLeft; //координаты клика внутри img
+    yInside = event.pageY - img.offsetTop - this.offsetTop;
   }
 });
 
 container.addEventListener('mousemove', function (event) {
   if (down) {
-    if (event.target.tagName === 'IMG') {
+    if (img.tagName === 'IMG') {
       event.preventDefault();
       // находим координаты клика внутри container и меняем положение img  
       var insideContainerX = event.pageX - this.offsetLeft - xInside;
       var insideContainerY = event.pageY - this.offsetTop - yInside;
-      event.target.style.left = insideContainerX + 'px';
-      event.target.style.top = insideContainerY + 'px';
+      img.style.left = insideContainerX + 'px';
+      img.style.top = insideContainerY + 'px';
       //огр. область перемещения картинок размером контейнера + меняем border у контейнера на красный
       addAlarm(insideContainerX, insideContainerY, this);
     }
@@ -48,28 +50,28 @@ container.addEventListener('mousemove', function (event) {
 container.addEventListener('mouseup', event => {
   event.preventDefault();
   down = false;
-  event.target.style.cursor = 'default';
+  img.style.cursor = 'default';
   imgs.forEach(item => {
     item.style.zIndex = 1;
   });
-  event.target.style.zIndex = 0; //картинка падает на самый нижний слой
+  img.style.zIndex = 0; //картинка падает на самый нижний слой
 });
 
 function addAlarm(x, y, container) {
   makeLimits('clientHeight', y, 'top', container);
   makeLimits('clientWidth', x, 'left', container);
-  if ((y > 0 && y < container.clientHeight - event.target.clientHeight) && (x > 0 && x < container.clientWidth - event.target.clientWidth)) {
+  if ((y > 0 && y < container.clientHeight - img.clientHeight) && (x > 0 && x < container.clientWidth - img.clientWidth)) {
     container.classList.remove('alarm');
   }
 }
 
 function makeLimits(limit, position, side, container) {
-  var heightLimit = container[limit] - event.target[limit];
+  var heightLimit = container[limit] - img[limit];
   if (position <= 0) {
-    event.target.style[side] = 0 + 'px';
+    img.style[side] = 0 + 'px';
     container.classList.add('alarm');
   } else if (position >= heightLimit) {
-    event.target.style[side] = heightLimit + 'px';
+    img.style[side] = heightLimit + 'px';
     container.classList.add('alarm');
   }
 }
